8527007d9e97a62f703c89acbe9cf42f3ab83e15
Agustin Goñi <agusgoni1997@gmail.com>
2022-09-22 12:51:46 -0300

    primeros componentes

--- app-coder/src/app/app.module.ts
+++ app-coder/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { ContenidoComponent } from './contenido/contenido.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    ToolbarComponent,
+    ContenidoComponent
   ],
   imports: [
     BrowserModule,
