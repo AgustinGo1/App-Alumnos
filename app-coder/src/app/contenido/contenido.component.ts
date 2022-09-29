2ce06518624c3076ee7a5ae1a9910345038532a8
Agustin Goñi <agusgoni1997@gmail.com>
2022-09-29 16:39:25 -0300

    incluir arrays

--- app-coder/src/app/contenido/contenido.component.ts
+++ app-coder/src/app/contenido/contenido.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContenidoComponent implements OnInit {
 
+  listaAlumnos = ['Pedro', 'Agustin', 'Andres', 'Jorge', 'Mariano', 'Roberto', 'Lucas', 'Luciano'];
+  listaVisualizados: any = [];
+
+  public alm: string = '';
+  public visto: boolean = false;
+
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+
+  }
+
+  public AddAlumno () {
+    if(this.listaAlumnos.length > 0){
+      this.alm = this.listaAlumnos[0];
+      this.listaVisualizados.push(this.alm);
+      this.listaAlumnos.shift();
+      if(this.listaVisualizados.length > 0){
+        this.visto = true;
+      }
+
+    }
   }
 
 }
