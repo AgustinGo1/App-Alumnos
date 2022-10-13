d383ed413893bff493d76164acf97774702d9859
Agus <agusgoni1997@gmail.com>
2022-10-12 21:07:16 -0300

    diseño login

--- app-alumnos/src/app/app.module.ts
+++ app-alumnos/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
@@ -20,7 +19,7 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- app-alumnos/src/app/signup/signup.component.ts
+++ app-alumnos/src/app/signup/signup.component.ts
@@ -34,7 +34,7 @@ export class SignupComponent implements OnInit {
 
 
   public iniciarSesion() {
-    console.log('kb', this.formSignup.value);
+    console.log('inicie sesion jeje');
 
   }
 
